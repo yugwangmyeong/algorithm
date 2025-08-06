579c00cdac4864e1d13a4696ab158a63854d2524
yugwangmyeong <ykm5922@naver.com>
2025-08-06 20:10:23 +0900

    백트래킹 기본문제

---
+++ algorithm/BACK TRACKING/BJ15650.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+rs = []
+
+def recur(num):
+    if len(rs) == M:
+        print(' '.join(map(str,rs)))
+        return
+    for i in range(num,N+1):
+        rs.append(i)
+        recur(i+1)
+        rs.pop()
+          
+                
+recur(1)
+
